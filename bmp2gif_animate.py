df8902703860c270316edbcaef4bff26526c17aa
snljty <snljty@gmail.com>
2020-09-09 12:23:50 +0800

    add comment

--- bmp2gif_animate.py
+++ bmp2gif_animate.py
@@ -2,10 +2,16 @@
 
 import imageio
 import os
+import time
 
+# if you want to reverse the animate, change the ireverse from False to True.
+ireverse = False
+
+t = time.time()
 images = list()
-filenames = sorted((fn for fn in os.listdir('.') if fn.endswith('.bmp')), reverse = False)
-for filename in filenames:
-    images.append(imageio.imread(filename))
+filenames = [fn for fn in os.listdir('.') if fn.endswith('.bmp')]
+filenames.sort(reverse = ireverse)
+for filename in filenames: images.append(imageio.imread(filename))
 imageio.mimsave('out.gif', images, duration = 0.1, loop = 1)
+print('Time elapsed: {:.1f} s'.format(time.time() - t))
 
