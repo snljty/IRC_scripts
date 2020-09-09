9f4e79104acc36bccce4cda95d9603d34424b417
snljty <snljty@gmail.com>
2020-09-09 11:51:40 +0800

    add render progress

--- render_all.bat
+++ render_all.bat
@@ -13,6 +13,7 @@ set ncores=2
 rem make sure the correct version of tachyon is used, 
 rem and it is either in PATH or with full path.
 for /f "usebackq delims=" %%i in (`dir /b *.dat`) do (
+    echo ############rendering %%~ni ...############
     tachyon_WIN64.exe %%i -format BMP -o %%~ni.bmp ^
 -trans_raster3d -res 1500 1500 -mediumshade -aasamples 24 -numthreads %ncores%
 )
